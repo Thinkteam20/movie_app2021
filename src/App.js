d322be61b0c6b23a6842de89b02b167650c01c74
Sangpil Jung <tankteam20@gmail.com>
2021-02-10 08:40:41 +1000

    added axious

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import React from "react";
+import axios from "axios";
+
+
+// 1. application is going to render 
 
 class App extends React.Component{
-  constructor(props){
-    super(props)
-    console.log("hello")
-  }
   state = {
     // this is the place where we put the data which will be changed.  ex ) count: 0
-    count: 0
+
+    //2. in the beginning isLoading is true 
+
+    isLoading: true,
+    movies:[]
+  };
+  //async = this function is async now it tells JS "you need to wait for it"
+  // 5. after mount get movie execute axios.get however, it take a time to get big json file data. so we put await
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // await tell wait axiout.get is finished then continue function 
+    //console.log({movies})
+    // 6. put data we want to movie[]
+    this.setState({movies, isLoading:false})
   };
 
-  add = () => {
-    //console.log("add");
-    this.setState(current => ({count: current.count + 1}))
-  }
-
-  minus = () =>{ 
-    //console.log("minus"); 
-    this.setState({count: this.state.count - 1 })
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, Cruel world")
-  }
+  //4. after the application is mount we call the this.getMovie() immediately
   componentDidMount() {
-    console.log("component rendered");
-  }
-  componentDidUpdate() {
-    console.log("I just update")
+    this.getMovies();
   }
   render(){
-    console.log("I'm rendering")
-    return <div>
-      <h1>The number is : {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
+    const { isLoading } = this.state;
+    // 3. so we going to see "Loading.."
+    return <div>{isLoading ? "Loading.." : "We are ready"}</div>
   }
 }
 
-export default App;
+export default App; 
